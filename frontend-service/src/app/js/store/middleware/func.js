6fedef5cff4de69707ef0135f8e5cd106d54624d
mayowa-ola <mayowaoladunjoye@gmail.com>
2020-09-05 13:57:32 +0100

    codacy again!!!

--- frontend-service/src/app/js/store/middleware/func.js
+++ frontend-service/src/app/js/store/middleware/func.js
@@ -2,8 +2,9 @@ const func = ({dispatch, getState}) => (next) => (action) => {
     if(typeof (action) === 'function') {
         action(dispatch, getState);
     }
-    else 
+    else {
         next(action);
+    }
 }
 
 export default func;
