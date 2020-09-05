b815ee1637da3d40a41b9cd6128b67965444adad
mayowa-ola <mayowaoladunjoye@gmail.com>
2020-09-05 14:27:39 +0100

    codacy

--- frontend-service/src/app/js/store/middleware/func.js
+++ frontend-service/src/app/js/store/middleware/func.js
@@ -2,7 +2,9 @@ const func = ({dispatch, getState}) => (next) => (action) => {
     if(typeof (action) === 'function') {
         action(dispatch, getState);
     }
-    else next(action);
+    else {
+        next(action)
+    };
 
 };
 
